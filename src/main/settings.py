78e4f5b6cd4a4025c6f0765290473b8c8e3c29cd
manilde <m.van.der.Oord@amsterdam.nl>
2023-07-31 18:04:17 +0200

    Revert "fix static_root"

--- src/main/settings.py
+++ src/main/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 
 import os
-from urllib.parse import urljoin
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -69,8 +68,8 @@ FORCE_SCRIPT_NAME = BASE_URL
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = urljoin(f"{BASE_URL}/", "static/")
-STATIC_ROOT = "static"
+STATIC_URL = os.path.join(BASE_URL, "/static/")
+STATIC_ROOT = "static/"
 
 
 TEMPLATES = [
