c6374a5ffb707323a2cff02ed7eda3bc97710d89
Manilde van der Oord <m.van.der.oord@amsterdam.nl>
2023-12-18 16:17:53 +0100

    add posability to delete gebied record

--- src/bereikbaarheid/admin.py
+++ src/bereikbaarheid/admin.py
@@ -150,9 +150,6 @@ class GebiedAdmin(ImportMixin, LeafletGeoAdminMixin, admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
-
 
 @admin.register(Lastbeperking)
 class LastbeperkingAdmin(ImportExportFormatsMixin, admin.ModelAdmin):
