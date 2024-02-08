d19212a7b688b15cf5debf93cccd5be9a9265743
Manilde van der Oord <m.van.der.oord@amsterdam.nl>
2024-02-08 16:00:11 +0100

    added test_auth for groups

---
+++ tests/test_touringcar/test_auth.py
@@ -0,0 +1,43 @@
+import pytest
+from django.contrib.auth.models import Group, User
+from model_bakery import baker
+
+from main.auth import OIDCAuthenticationBackend
+
+GROUPNAME = ['bereikbaarheid', 'touringcar']
+
+@pytest.fixture
+def django_group_bereikbaarheid():
+    return baker.make(Group, name= GROUPNAME[0])
+
+@pytest.fixture
+def django_group_touringcar():
+    return baker.make(Group, name= GROUPNAME[1])
+
+class Test_auth:
+
+    @pytest.mark.parametrize(
+        "key, test_claims, expected",
+        [
+            (1,  {'name': 'test', 'family_name': 'family_test', 'given_name': 'Test', 'email': 'test@amsterdam.nl', 'roles': ['o-bereikbaarheid-application-admin']},
+                "<QuerySet []>",
+             ),
+            (2, {'name': 'test', 'family_name': 'family_test', 'given_name': 'Test', 'email': 'test@amsterdam.nl', 'roles': ['o-bereikbaarheid-app-admin-bereikbaarheid']},
+                "<QuerySet ['bereikbaarheid']>",
+             ),
+            (3,  {'name': 'test', 'family_name': 'family_test', 'given_name': 'Test', 'email': 'test@amsterdam.nl', 'roles': ['o-bereikbaarheid-app-admin-touringcar']},
+                "<QuerySet ['touringcar']>",
+             ),
+            (4, {'name': 'test', 'family_name': 'family_test', 'given_name': 'Test', 'email': 'test@amsterdam.nl', 'roles': ['o-bereikbaarheid-app-admin-bereikbaarheid','o-bereikbaarheid-app-admin-touringcar']},
+                "<QuerySet ['bereikbaarheid', 'touringcar']>",
+             ),
+        ],
+    )
+    @pytest.mark.django_db
+    def test_update_group(self, django_group_bereikbaarheid, django_group_touringcar, key, test_claims, expected):
+        OIDC = OIDCAuthenticationBackend()
+        OIDC.create_user(test_claims)
+
+        user = User.objects.get(pk=key)
+        assert user.last_name == "family_test"
+        assert str(user.groups.values_list('name',flat = True)) == expected   
